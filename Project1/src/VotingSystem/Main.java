3cb3fa68dd17f9fdff56f49629352ba0672eab48
Jonathan Haak <haak0052@umn.edu>
2023-03-13 14:07:12 -0500

    Code Runs LMAO

--- Project1/src/VotingSystem/Main.java
+++ Project1/src/VotingSystem/Main.java
@@ -3,7 +3,10 @@ package VotingSystem;
 public class Main {
 
     public static void main(String args []) {
-        String holder = args[0];
+        String holder = " ";
+        if(args.length > 0){
+            holder = args[0];
+        }   
         RunElection newElection = new RunElection(holder);
         newElection.start();
 
