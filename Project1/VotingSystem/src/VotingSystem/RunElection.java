2644be4c8736824fdb7d54ae91e0af5e48695a75
Jonathan Haak <haak0052@umn.edu>
2023-03-21 21:36:16 -0500

    Some stuff

--- Project1/VotingSystem/src/VotingSystem/RunElection.java
+++ Project1/VotingSystem/src/VotingSystem/RunElection.java
@@ -1,16 +1,19 @@
 package VotingSystem;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
+
 import java.util.ArrayList;
 import java.util.*;
 
 public class RunElection {
-    public String filename = "";
+    public String filename;
     private FileReader input;
     private FileWriter audit;
     private BufferedReader br;
@@ -20,25 +23,31 @@ public class RunElection {
         boolean validFile = false;
         while (!validFile) {
             validFile = true;
-            if(filename.equals("")){
+            if(filename == null){
                 promptUser("Enter the name of the file containing the election data: ");
                 Scanner sc = new Scanner(System.in);
 
                 filename = sc.nextLine();
+                System.out.println(filename);
             }
             try {
-                input = new FileReader(filename);
+                URL path = RunElection.class.getResource(filename);
+                File f = new File(path.getFile());
+                input = new FileReader(f);
             } catch (FileNotFoundException e) {
                 validFile = false;
                 promptUser("Error, File not found" + 
                     "\nPlease ensure the file is in the same directory as the program\n" +
-                    "The exected input is filename.csv, please try again");
+                    "The expected input is filename.csv, please try again");
+                filename = null;
             }
         }
         String elecType = parseElectType();
         String auditFileName = generateAuditFileName(elecType);
         try {
-            audit = new FileWriter(auditFileName);
+            String path = System.getProperty("user.dir");
+            File f = new File(path, auditFileName);
+            audit = new FileWriter(f);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -64,7 +73,8 @@ public class RunElection {
     }
     public String generateAuditFileName(String elecType){
         Date d1 = new Date();
-        return elecType + d1 + ".txt";
+        String d2 = "dummy";
+        return elecType + d2 + ".txt";
     }
     public String parseElectType(){
         String line = "";
