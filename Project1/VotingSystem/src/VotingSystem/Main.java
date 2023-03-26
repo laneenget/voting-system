7f7e357bb4b1787a7c6bed256089493ea7521ba1
Jonathan Haak <jonathan.haaka@gmail.com>
2023-03-26 12:53:07 -0500

    Comments in Main

--- Project1/VotingSystem/src/VotingSystem/Main.java
+++ Project1/VotingSystem/src/VotingSystem/Main.java
@@ -1,14 +1,22 @@
 package VotingSystem;
 
 public class Main {
+/**
+ * The main method of the program.
+ *
+ * @param args The command line arguments.
+ */
+public static void main(String args[]) {
+    String holder = null;
 
-    public static void main(String args []) {
-        String holder = null;
-        if(args.length > 0){
-            holder = args[0];
-        }   
-        RunElection newElection = new RunElection(holder);
-        newElection.start();
-
+    // Check if command line argument(s) are provided
+    if (args.length > 0) {
+        holder = args[0];
     }
+
+    // Create a new RunElection object and start the election
+    RunElection newElection = new RunElection(holder);
+    newElection.start();
+}
+
 }
