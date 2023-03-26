d798b8e1d41c37f5f25c4011eda2cc76b6ee8c6a
Jonathan Haak <haak0052@umn.edu>
2023-03-26 15:55:57 -0500

    MainTest Comments

--- Project1/src/VotingSystem/MainTest.java
+++ Project1/src/VotingSystem/MainTest.java
@@ -9,19 +9,31 @@ import org.junit.Test;
 
 
 import static org.junit.Assert.fail;
+/*
+ * This class is used to test the RunElection class
+ * Through which the entire system functionality
+ * is tested.
+ * Results are validated via command line
+ * output and validating the creation of
+ * an accurate audit file.
+ */
 public class Maintests {
     
 
 
     @Test
     public void testCPL(){
+        // Create new RunElection object with "france.csv" as the file name
         RunElection newElec = new RunElection("france.csv");
+        // Call start method of election
         newElec.start();
     }
 
     @Test
     public void testIR(){
+        // Create new RunElection object with "ballot.csv" as the file name
         RunElection newElec = new RunElection("ballot.csv");
+        // Call start method of election
         newElec.start();
     }
 }
