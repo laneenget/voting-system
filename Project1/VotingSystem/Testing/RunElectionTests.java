ca41c99d280210047c200bdf713fea6b2368c15c
Jonathan Haak <jonathan.haaka@gmail.com>
2023-03-25 19:22:43 -0500

    Some stuff

--- Project1/VotingSystem/Testing/RunElectionTests.java
+++ Project1/VotingSystem/Testing/RunElectionTests.java
@@ -1,5 +1,93 @@
 package Testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import VotingSystem.CPL;
+import VotingSystem.IR;
+import VotingSystem.RunElection;
+
 public class RunElectionTests {
     
+    private RunElection election;
+    private File file;
+    private FileWriter audit;
+    
+    @BeforeEach
+    public void setup() throws IOException {
+        URL path = RunElection.class.getResource("/test.csv");
+        file = new File(path.getFile());
+        audit = mock(FileWriter.class);
+        election = new RunElection(file.getName());
+    }
+    
+    @Test
+    public void testGenerateAuditFileName() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+        String expectedIRName = "IR" + now.format(formatter) + ".txt";
+        String expectedCPLName = "CPL" + now.format(formatter) + ".txt";
+        
+        String actualIRName = election.generateAuditFileName("IR");
+        String actualCPLName = election.generateAuditFileName("CPL");
+        
+        assertEquals(expectedIRName, actualIRName);
+        assertEquals(expectedCPLName, actualCPLName);
+    }
+    
+    @Test
+    public void testParseElectType() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String expectedType = br.readLine();
+        br.close();
+        
+        String actualType = election.parseElectType();
+        
+        assertEquals(expectedType, actualType);
+    }
+    
+    @Test
+    public void testRunIR() throws IOException {
+        IR ir = mock(IR.class);
+        election.runIR();
+    }
+    
+    @Test
+    public void testRunCPL() throws IOException {
+        CPL cpl = mock(CPL.class);
+        election.runCPL();
+    }
+    
+    @Test
+    public void testStartWithValidFile() throws IOException {
+        RunElection spyElection = org.mockito.Mockito.spy(election);
+        
+        spyElection.start();
+        
+        org.mockito.Mockito.verify(spyElection).runIR();
+    }
+    
+    @Test
+    public void testStartWithInvalidFile() throws IOException {
+        election = new RunElection("invalid.csv");
+        RunElection spyElection = org.mockito.Mockito.spy(election);
+        
+        assertThrows(NullPointerException.class, () -> {
+            spyElection.start();
+        });
+    }
 }
+
--- Project1/VotingSystem/src/VotingSystem/Election.java
+++ Project1/VotingSystem/src/VotingSystem/Election.java
@@ -11,6 +11,13 @@ public abstract class Election {
     protected FileReader input;
 
     abstract public void processFile();
+    /*
+     * This method is used to break ties in the election. It takes in an arraylist of candidates
+     * and the number of candidates to eliminate. It then randomly selects candidates to eliminate
+     * @param candidates ArrayList of candidates to break the tie
+     * @param numToEliminate number of candidates to eliminate
+     * @return int[] of the candidates to eliminate
+     */
     public int [] breakTie(ArrayList<Integer> candidates, int numToEliminate){
         if(candidates.size() <= numToEliminate){
             return candidates.stream()
